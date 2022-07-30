5e443fdc5963501a3743ec952b94974d637fc031
Il Harper <hi@ilharper.com>
2022-07-31 01:39:40 +0800

    feat: add go code

---
+++ colette.go
@@ -0,0 +1,26 @@
+package colette
+
+//#cgo CFLAGS: -I${SRCDIR}/include/colette
+//#cgo LDFLAGS: -L${SRCDIR}/include/colette -lcolette
+//#include "colette.h"
+import "C"
+
+func RgbTo256(r rune, g rune, b rune) int {
+	return C.colette_rgbto256(r, g, b)
+}
+
+func JoinRgb(r rune, g rune, b rune) int {
+	return C.colette_join_rgb(r, g, b)
+}
+
+func SplitRgb(c int, r *rune, g *rune, b *rune) {
+	C.colette_split_rgb(c, r, g, b)
+}
+
+func Color256ToRgb(c int) int {
+	return C.colette_256torgb(c)
+}
+
+func Color256To16(c int) int {
+	return C.colette_256to16(c)
+}
---
+++ colette_test.go
@@ -0,0 +1,46 @@
+package colette
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRgbTo256(t *testing.T) {
+	assert.Equal(t, 16, RgbTo256(0, 0, 0), "Black (0 / 16)")
+	assert.Equal(t, 231, RgbTo256(255, 255, 255), "White (15 / 231)")
+	assert.Equal(t, 196, RgbTo256(255, 0, 0), "Red (196)")
+	assert.Equal(t, 46, RgbTo256(0, 255, 0), "Green (46)")
+	assert.Equal(t, 21, RgbTo256(0, 0, 255), "Blue (21)")
+	assert.Equal(t, 251, RgbTo256(200, 200, 200), "Grey (232 - 255)")
+	assert.Equal(t, 251, RgbTo256(201, 201, 201), "Fuzzy match")
+}
+
+func TestJoinRgb(t *testing.T) {
+	assert.Equal(t, 0x000000, JoinRgb(0, 0, 0), "Black")
+	assert.Equal(t, 0xffffff, JoinRgb(255, 255, 255), "White")
+	assert.Equal(t, 0xff0000, JoinRgb(255, 0, 0), "Red")
+	assert.Equal(t, 0x00ff00, JoinRgb(0, 255, 0), "Green")
+	assert.Equal(t, 0x0000ff, JoinRgb(0, 0, 255), "Blue")
+	assert.Equal(t, 0xc8c8c8, JoinRgb(200, 200, 200), "Grey")
+}
+
+func TestSplitRgb(t *testing.T) {
+	var r, g, b rune
+	SplitRgb(0xc8c8c8, &r, &g, &b)
+	assert.Equal(t, 200, r, "R")
+	assert.Equal(t, 200, g, "G")
+	assert.Equal(t, 200, b, "B")
+}
+
+func TestColor256ToRgb(t *testing.T) {
+	assert.Equal(t, JoinRgb(135, 255, 135), Color256ToRgb(120), "Custom Color 1")
+	assert.Equal(t, JoinRgb(215, 255, 0), Color256ToRgb(190), "Custom Color 2")
+}
+
+func TestColor256To16(t *testing.T) {
+	assert.Equal(t, 12, Color256To16(21), "Pure Blue")
+	assert.Equal(t, 1, Color256To16(52), "Deep Red")
+	assert.Equal(t, 9, Color256To16(196), "Pure Red")
+	assert.Equal(t, 7, Color256To16(244), "Normal Grey")
+	assert.Equal(t, 15, Color256To16(253), "Light Grey")
+}
