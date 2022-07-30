c22fbba0eb9b0f01c52801703084fbc2139f013e
Il Harper <hi@ilharper.com>
2022-07-31 02:28:33 +0800

    fix: fix `SplitRgb()` method

--- colette.go
+++ colette.go
@@ -1,8 +1,10 @@
 package colette
 
-//#cgo CFLAGS: -I${SRCDIR}/include/colette
-//#include "colette.h"
+//#cgo CFLAGS: -I${SRCDIR}/include
+//#include "colette/colette.h"
 import "C"
+
+import "unsafe"
 
 func RgbTo256(r rune, g rune, b rune) int {
 	return int(C.colette_rgbto256(C.uchar(r), C.uchar(g), C.uchar(b)))
@@ -13,7 +15,12 @@ func JoinRgb(r rune, g rune, b rune) int {
 }
 
 func SplitRgb(c int, r *rune, g *rune, b *rune) {
-	C.colette_split_rgb(C.int(c), C.uchar(r), C.uchar(g), C.uchar(b))
+	C.colette_split_rgb(
+		C.int(c),
+		(*C.uchar)(unsafe.Pointer(r)),
+		(*C.uchar)(unsafe.Pointer(g)),
+		(*C.uchar)(unsafe.Pointer(b)),
+	)
 }
 
 func Color256ToRgb(c int) int {
